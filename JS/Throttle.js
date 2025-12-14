4f83d49e5af5813895b89aedd5cab929edb504d6
raut-abhishek <iamabhishekraut01@gmail.com>
2025-12-14 22:42:34 +0530

    Throttle

---
+++ JS/Throttle.js
@@ -0,0 +1,17 @@
+
+
+function throttle(fn, limit) {
+  let inThrottle = false;
+  return function (...args) {
+    if (!inThrottle) {
+      fn.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
+    }
+  };
+}
+
+// Usage
+const logScroll = throttle(() => {
+  console.log("Scrolling...");
+}, 1000);
