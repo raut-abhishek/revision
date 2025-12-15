554585602c9d89963a68f62f6c5498690c4001f5
raut-abhishek <iamabhishekraut01@gmail.com>
2025-12-15 23:11:53 +0530

    React app setup

---
+++ react/testing-revision/concept-testing/src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import UserData from './concepts/props/UserData';
+import UseStateExample from './concepts/usestate/UseStateExample';
+import ButtonClick from './concepts/event-handling/ButtonClick';
+import HandlingTextInput from './concepts/event-handling/HandlingTextInput';
+import ConditionalRendering from './concepts/conditional-rendering/ConditionalRendering.jsx';
+import RenderNameList from './concepts/list-rendering/RenderNameList.jsx';
+import Parent from './concepts/Component communication/child to parent/Parent.jsx';
+import Parent1 from './concepts/Component communication/Parent to Child/Parent1.jsx'
+import Forms1 from './concepts/Controlled Forms/Forms1.jsx';
+import ApiTesting from './concepts/API testing/ApiTesting.jsx'
+import LoadindAndErrorStates from './concepts/Loading and Error States/LoadindAndErrorStates.jsx';
+import LoadindAndErrorStates1 from './concepts/Loading and Error States/LoadindAndErrorStates1.jsx';
+import Pagination from './concepts/pagination/Pagination.jsx';
+import Header from './concepts/context API/Header.jsx';
+import ThemeDemo from "./concepts/context API/ThemeDemo";
+import ReactMemo from './concepts/Performance optimization/ReactMemo.jsx';
+import UseMemo from './concepts/Performance optimization/UseMemo.jsx';
+import UseCallback from './concepts/Performance optimization/UseCallback.jsx'
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <UserData/> */}
+      {/* <UseStateExample/> */}
+      {/* <ButtonClick/> */}
+      {/* <HandlingTextInput/> */}
+      {/* <ConditionalRendering/> */}
+      {/* <RenderNameList/> */}
+      {/* <Parent/> */}
+      {/* <Parent1/> */}
+      {/* <Forms1/> */}
+      {/* <ApiTesting/> */}
+      {/* <LoadindAndErrorStates/> */}
+      {/* <LoadindAndErrorStates1/> */}
+      {/* <Pagination/> */}
+      {/* <Header/> */}
+      {/* <ThemeDemo/> */}
+      {/* <ReactMemo/> */}
+      {/* <UseMemo/> */}
+      <UseCallback/>
+
+
+    </div>
+  )
+}
+
+export default App;
