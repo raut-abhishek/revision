047c2555cb167326977d54c6bf42a4bc8ef095f9
raut-abhishek <iamabhishekraut01@gmail.com>
2025-12-18 20:37:36 +0530

    schema model changed

--- react/testing-revision/concept-testing/src/concepts/MongoDB/schemaDesign.js
+++ react/testing-revision/concept-testing/src/concepts/MongoDB/schemaDesign.js
@@ -1,22 +1,37 @@
 // Schema Design: Embedded vs Referenced Documents in MongoDB
 
+const mongoose = require('mongoose');
+
 
 // Embedded schema
 
-const embeddedSchema = {
-  _id: 1,
-  name: "Abhishek",
-  orders: [
-    { orderId: 101, total: 500 },
-    { orderId: 102, total: 1200 }
-  ]
-}
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String,
+  pincode: Number
+});
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  address: addressSchema
+});
+
+module.exports = mongoose.model("User", userSchema);
+
+
 
 
 // Referenced schema
 
-const referencedSchema = [
-  { _id: 1, name: "Abhishek" },
-  { orderId: 101, userId: 1, total: 500 },
-  { orderId: 102, userId: 1, total: 1200 }
-]
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  amount: Number,
+  status: String
+});
+
+module.exports = mongoose.model("Order", orderSchema);
