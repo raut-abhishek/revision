c1a6650ea1d3b70769b295095852e25a4173a859
raut-abhishek <iamabhishekraut01@gmail.com>
2025-12-19 17:34:22 +0530

    login controlller

---
+++ react/testing-revision/concept-testing/src/concepts/MongoDB/loginconroller.js
@@ -0,0 +1,22 @@
+import { sign } from "jsonwebtoken";
+
+export async function login(req, res) {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
+
+  const token = sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true
+  }).json({ success: true });
+}
