51e045896119d34c59a1ac71db9f824763493dd3
raut-abhishek <iamabhishekraut01@gmail.com>
2025-12-25 23:04:29 +0530

    sort arraylist

---
+++ java/DSA/arrayList/SortList.java
@@ -0,0 +1,17 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(30);
+        list.add(10);
+        list.add(20);
+
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
